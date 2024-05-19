22ed9472b1726af663b7a21b8fb60b77a9f55e62
KG ARUN KUMAR KA19SDF180254 <arunkumarshendra@gmail.com>
2024-05-19 23:02:42 +0530

    third commit

--- SPORTS manage/Login.cs
+++ SPORTS manage/Login.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace SPORTS_manage
 {
@@ -15,6 +16,36 @@ namespace SPORTS_manage
         public Login()
         {
             InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection ("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"Z:\\Sports\\SPORTS manage\\Sports Database.mdf\";Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        private String getUserName()
+        {
+            // fetch the data from database
+            conn.Open ();
+            String syntax = "SELECT Value from loginTable where Property = 'UserName' ";
+            cmd = new SqlCommand (syntax, conn);
+            dr = cmd.ExecuteReader ();
+            dr.Read ();
+            String temp = dr[0].ToString();
+            conn.Close();
+            return temp;
+        }
+
+        private String getPassword()
+        {
+            // fetch the data from database
+            conn.Open();
+            String syntax = "SELECT Value from loginTable where Property = 'Password' ";
+            cmd = new SqlCommand(syntax, conn);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            String temp = dr[0].ToString();
+            conn.Close();
+            return temp;
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -29,7 +60,7 @@ namespace SPORTS_manage
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String Uname = "Arun Kumar", Upass = "Amazon.in@123**", name, pass;
+            String Uname = getUserName(), Upass = getPassword(), name, pass;
             name = textBox1.Text;
             pass = textBox2.Text;
 
